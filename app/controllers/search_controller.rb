03a4c419ded7183f08262ce04667a633c680b09c
Megan Arellano <ssciolist@gmail.com>
2018-05-16 10:13:21 -0600

    can consume api and output page results. Need to refine api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,13 @@
 class SearchController < ApplicationController
   def index
     conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{params[:q]}")
-    conn.get
-    binding.pry
+    body = conn.get.body
+
+    raw_results = JSON.parse(body, symbolize_names: true)
+    raw_results[:fuel_stations].map do |station|
+      Station.new(station)
+    end
+
+    @stations = Station.all
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,14 @@
 class Station
-  def self.search_results(zipcode)
-    
-    binding.pry
+  attr_reader :name, :address, :fuel_types, :distance, :access_times
+  def initialize(attrs)
+    @name = attrs[:station_name]
+    @address = attrs[:street_address]
+    @fuel_types = attrs[:fuel_type_code]
+    @distance = attrs[:distance]
+    @access_times = attrs[:access_days_time]
+  end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
   end
 end
